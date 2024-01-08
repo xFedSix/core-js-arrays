b4fadb74d804fb9491a0394a0ed11d06bf6e0361
Fiodar Siyanka <siyankafiodar@gmail.com>
2024-01-08 13:32:27 +0300

    feat: findAllOccurrences

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -78,8 +78,9 @@ function findElement(arr, value) {
  *    findAllOccurrences([ null, undefined, null ], null) => 2
  *    findAllOccurrences([ true, 0, 1, 'true' ], true) => 1
  */
-function findAllOccurrences(/* arr, item */) {
-  throw new Error('Not implemented');
+function findAllOccurrences(arr, item) {
+  // throw new Error('Not implemented');
+  return arr.filter((occur) => occur === item).length;
 }
 
 /**
