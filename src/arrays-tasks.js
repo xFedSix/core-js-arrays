e798ce0275e85bf17dcd66e1a9285dc28c7476bc
Fiodar Siyanka <siyankafiodar@gmail.com>
2024-01-08 13:37:15 +0300

    feat: removeFalsyValues

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -95,8 +95,9 @@ function findAllOccurrences(arr, item) {
  *    removeFalsyValues([ 1, 2, 3, 4, 5, 'false' ]) => [ 1, 2, 3, 4, 5, 'false' ]
  *    removeFalsyValues([ false, 0, NaN, '', undefined ]) => [ ]
  */
-function removeFalsyValues(/* arr */) {
-  throw new Error('Not implemented');
+function removeFalsyValues(arr) {
+  // throw new Error('Not implemented');
+  return arr.filter((falsy) => !!falsy);
 }
 
 /**
