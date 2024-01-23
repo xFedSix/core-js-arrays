76aa12527132377eda560fb834d02cb9b3781630
Fiodar Siyanka <siyankafiodar@gmail.com>
2024-01-23 22:51:41 +0300

    feat: add flattenArray

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -304,8 +304,9 @@ function createNDimensionalArray(n, size) {
  *    flattenArray(['a', ['b', ['c', 'd'], 'e'], 'f']) => ['a', 'b', 'c', 'd', 'e', 'f']
  *    flattenArray([1, 2, 3, 4]) => [1, 2, 3, 4]
  */
-function flattenArray(/* nestedArray */) {
-  throw new Error('Not implemented');
+function flattenArray(nestedArray) {
+  // throw new Error('Not implemented');
+  return nestedArray.flat(Infinity);
 }
 
 /**
