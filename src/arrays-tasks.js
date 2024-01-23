380b4fffb0448d6a20c32992c2ac0f450ed052cd
Fiodar Siyanka <siyankafiodar@gmail.com>
2024-01-23 22:48:25 +0300

    feat: add createNDimensionalArray

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -285,8 +285,12 @@ function distinct(arr) {
  *    createNDimensionalArray(4, 2) => [[[[0, 0], [0, 0]], [[0, 0], [0, 0]]], [[[0, 0], [0, 0]], [[0, 0], [0, 0]]]]
  *    createNDimensionalArray(1, 1) => [0]
  */
-function createNDimensionalArray(/* n, size */) {
-  throw new Error('Not implemented');
+function createNDimensionalArray(n, size) {
+  // throw new Error('Not implemented');
+  if (n === 1) {
+    return new Array(size).fill(0);
+  }
+  return new Array(size).fill().map(() => createNDimensionalArray(n - 1, size));
 }
 
 /**
