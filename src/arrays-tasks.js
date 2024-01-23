df7b4aa99a4bf4b69b8566686d8efeb84899f05e
Fiodar Siyanka <siyankafiodar@gmail.com>
2024-01-23 22:32:25 +0300

    feat: add doubleArray

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -234,8 +234,9 @@ function getTail(arr, n) {
  *    doubleArray([0, 1, 2, 3, 4, 5]) => [0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5]
  *    doubleArray([]) => []
  */
-function doubleArray(/* arr */) {
-  throw new Error('Not implemented');
+function doubleArray(arr) {
+  // throw new Error('Not implemented');
+  return arr.concat(arr);
 }
 
 /**
