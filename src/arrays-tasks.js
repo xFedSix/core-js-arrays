e44b96064021ad4d6d1a9907e84ea97fc67675c9
Fiodar Siyanka <siyankafiodar@gmail.com>
2024-01-23 22:30:23 +0300

    feat: add getTail

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -214,8 +214,12 @@ function getHead(arr, n) {
  *    getTail([ 'a', 'b', 'c', 'd'], 3) => [ 'b', 'c', 'd' ]
  *    getTail([ 'a', 'b', 'c', 'd'], 0) => []
  */
-function getTail(/* arr, n */) {
-  throw new Error('Not implemented');
+function getTail(arr, n) {
+  // throw new Error('Not implemented');
+  if (n === 0) {
+    return [];
+  }
+  return arr.slice(-n);
 }
 
 /**
