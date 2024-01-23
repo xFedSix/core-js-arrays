84acf7ecd2b3a09dbeb70412101494ba8e17b777
Fiodar Siyanka <siyankafiodar@gmail.com>
2024-01-23 22:34:06 +0300

    feat: add toStringList

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -250,8 +250,9 @@ function doubleArray(arr) {
  *    toStringList([1, 2, 3, 4, 5]) => '1,2,3,4,5'
  *    toStringList(['rock', 'paper', 'scissors']) => 'rock,paper,scissors'
  */
-function toStringList(/* arr */) {
-  throw new Error('Not implemented');
+function toStringList(arr) {
+  // throw new Error('Not implemented');
+  return arr.join(',');
 }
 
 /**
