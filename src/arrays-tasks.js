3489ea7b439765595b2daf9ed6e0eb9ca566092f
Fiodar Siyanka <siyankafiodar@gmail.com>
2024-02-09 20:46:11 +0300

    feat: add selectMany

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -322,8 +322,9 @@ function flattenArray(nestedArray) {
  *   selectMany([[1, 2], [3, 4], [5, 6]], (x) => x) =>   [ 1, 2, 3, 4, 5, 6 ]
  *   selectMany(['one','two','three'], (x) => x.split('')) =>   ['o','n','e','t','w','o','t','h','r','e','e']
  */
-function selectMany(/* arr, childrenSelector */) {
-  throw new Error('Not implemented');
+function selectMany(arr, childrenSelector) {
+  // throw new Error('Not implemented');
+  return arr.flatMap(childrenSelector);
 }
 
 /**
