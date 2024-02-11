69040fbe15e598db1b2333a1d6ee1b1d4d17b91e
Fiodar Siyanka <siyankafiodar@gmail.com>
2024-02-11 21:24:22 +0300

    feat: add generateOdds

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -385,8 +385,9 @@ function createChunks(arr, chunkSize) {
  *    generateOdds(2) => [ 1, 3 ]
  *    generateOdds(5) => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  // throw new Error('Not implemented');
+  return Array.from({ length: len }, (_, i) => 2 * i + 1);
 }
 
 /**
